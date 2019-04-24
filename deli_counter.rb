c0f11d1e0c1ba65f17afb1eeffafa01dbbebc66c
Andrew Silva <andrewsilva9@gmail.com>
2019-04-24 03:12:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,5 +14,5 @@ end
 
 def take_a_number(katz_deli, person)
   katz_deli << person
-  puts "Welcome #{person}. You are number #{katz_deli.length} in line."
+  puts "Welcome, #{person}. You are number #{katz_deli.length} in line."
 end
